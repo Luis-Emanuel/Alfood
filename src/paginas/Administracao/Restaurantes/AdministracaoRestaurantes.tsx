f7e2c819af1136265a9356049f34a02e51038fa7
L_Emanuel <43679743+Luis-Emanuel@users.noreply.github.com>
2025-04-10 16:00:58 -0300

    Implementa exclusão de restaurantes na área administrativa
    Adicionado botão “Excluir” ao lado de cada restaurante na tabela de administração.
    
    Criada função excluir que realiza requisição DELETE via Axios para a API v2.
    
    Lista de restaurantes no estado é atualizada automaticamente após exclusão.

--- src/paginas/Administracao/Restaurantes/AdministracaoRestaurantes.tsx
+++ src/paginas/Administracao/Restaurantes/AdministracaoRestaurantes.tsx
@@ -1,4 +1,4 @@
-import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
+import { Button, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
 import { useEffect, useState } from "react";
 import IRestaurante from "../../../interfaces/IRestaurante";
 import axios from "axios";
@@ -12,6 +12,14 @@ const AdministracaoRestaurantes = () => {
     axios.get<IRestaurante[]>("http://localhost:8000/api/v2/restaurantes/")
       .then(resposta => setRestaurantes(resposta.data))
   }, [])
+
+  const excluir = (restauranteASerExcluido: IRestaurante) => {
+    axios.delete(`http://localhost:8000/api/v2/restaurantes/${restauranteASerExcluido.id}/`)
+      .then(() => {
+        const listaAtualizada = restaurantes.filter(restaurante => restaurante.id !== restauranteASerExcluido.id);
+        setRestaurantes(listaAtualizada);
+      })
+  }
 
   return <TableContainer component={Paper}>
     <Table>
@@ -33,6 +41,9 @@ const AdministracaoRestaurantes = () => {
             </TableCell>
             <TableCell>
               [<Link to={`/admin/restaurantes/${restaurante.id}`}>editar</Link>]
+            </TableCell>
+            <TableCell>
+              <Button variant="outlined" color="error" onClick={() => excluir(restaurante)}>Excluir</Button>
             </TableCell>
           </TableRow>
         })}
